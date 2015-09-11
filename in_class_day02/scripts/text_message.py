65a187d66cd84bc0f7bb9e7aa370603d9d4fd1ae
Claire Keum <ayoung.keum@students.olin.edu>
2015-09-11 14:21:14 -0400

    inclass exercise sep10

---
+++ in_class_day02/scripts/text_message.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+""" Exploring publishing topics inside a Python node """
+
+from geometry_msgs.msg import PointStamped, Point
+from std_msgs.msg import Header
+
+point_msg = Point(x=1.0, y=2.0)
+header_msg = Header(stamp=rospy.Time.now(), frame_id='odom')
+point_stamped_msg = PointStamped(header=header_msg,
+								point=point_msg)
+
+pub = rospy.Publisher("/my_point", PointStamped, queue_size=10)
+pub.publish(point_stamped_msg)
+print point_stamped_msg
+
+r = rospy.Rate(10)
+while not rospy.is_shutdown():
+	pub.publish(point_stamped_msg)
+	r.sleep()
